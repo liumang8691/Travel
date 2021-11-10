4a6370c085f0aed218aa40e1493ebda1ef22af3e
liumang198691 <wanming88888@126.com>
2021-11-10 23:28:40 +0800

    add detail-header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from "./compontents/Banner.vue";
+import DetailHeader from "./compontents/Header.vue";
 export default {
   data() {
     return {};
   },
-  components: { DetailBanner },
+  components: { DetailBanner, DetailHeader },
   computed: {},
   methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
+.content {
+  height: 50rem;
+}
 </style>
---
+++ src/pages/detail/compontents/Header.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <span class="iconfont header-abs-back">&#xe743;</span>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link tag="div" to="/">
+        <span class="iconfont header-fixed-back">&#xe743;</span>
+      </router-link>
+      景点详情
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0,
+      },
+    };
+  },
+  activated: function () {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  components: {},
+  computed: {},
+  methods: {
+    handleScroll() {
+      const top = document.documentElement.scrollTop;
+      if (top > 60) {
+        let opacity = top / 140;
+        opacity = opacity > 1 ? 1 : opacity;
+        this.opacityStyle = {
+          opacity,
+        };
+        this.showAbs = false;
+      } else {
+        this.showAbs = true;
+      }
+      console.log(document.documentElement.scrollTop);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~styles/varibles.scss";
+
+.header-abs {
+  position: absolute;
+  left: 0.2rem;
+  top: 0.2rem;
+  width: 0.8rem;
+  height: 0.8rem;
+  line-height: 0.8rem;
+  border-radius: 50%;
+  text-align: center;
+  background: rgba(0, 0, 0, 0.8);
+  &-back {
+    color: #fff;
+    font-size: 0.4rem;
+  }
+}
+
+.header-fixed {
+  height: $headerHeight;
+  line-height: $headerHeight;
+  text-align: center;
+  color: #fff;
+  font-size: 0.32rem;
+  background: $bgColor;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  &-back {
+    position: absolute;
+    left: 0;
+    top: 0;
+    font-size: 0.4rem;
+    color: #fff;
+    width: 0.64rem;
+    text-align: center;
+  }
+}
+</style>
