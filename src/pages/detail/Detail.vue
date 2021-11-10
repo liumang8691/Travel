d28850840468b2a20e96a0f1de832ee9a4b90bab
liumang198691 <wanming88888@126.com>
2021-11-10 20:33:41 +0800

    detail-banner

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <detail-banner></detail-banner>
+  </div>
+</template>
+
+<script>
+import DetailBanner from "./compontents/Banner.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: { DetailBanner },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/pages/detail/compontents/Banner.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="banner">
+    <img
+      class="banner-img"
+      src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg"
+    />
+    <div class="banner-info">
+      <div class="banner-title">大连圣亚海洋世界（AAA景区）</div>
+      <div class="banner-number">
+        <span class="iconfont bannner-icon">&#xe63e;</span>39
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.banner {
+  position: relative;
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 55%;
+  &-img {
+    width: 100%;
+  }
+  &-info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    line-height: 0.6rem;
+    color: #fff;
+    background-image: linear-gradient(
+      top,
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 0.8)
+    );
+    .banner-title {
+      flex: 1;
+      font-size: 0.32rem;
+      padding: 0 0.2rem;
+    }
+    .banner-number {
+      padding: 0 0.4rem;
+      line-height: 0.32rem;
+      height: 0.32rem;
+      border-radius: 0.2rem;
+      background: rgba(0, 0, 0, 0.8);
+      font-size: 0.24rem;
+      .bannner-icon {
+        font-size: 0.24rem;
+        margin-right: 0.2rem;
+      }
+    }
+  }
+}
+</style>
